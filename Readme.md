ee1d1b6490ea1c7cd5e44648dccbb1acb94641cf
prashant patil <prashantpatil85@gmail.com>
2021-03-07 11:58:12 +0530

    initial version

---
+++ Readme.md
@@ -0,0 +1,59 @@
+# py App
+Flask API
+
+# python venv venv
+# pip install flask
+# pip install flask_dotenv
+# to resolve cros
+# pip install flask_cors
+
+# add in .flaskenv
+# FLASK_APP=api.py
+# FLASK_ENV=development
+
+# to run the app run
+flask run
+
+# Get all employee
+curl -X GET http://127.0.0.1:5000/api/v1/emp
+<!-- {
+  "E001": {"id": "E001", "last_name": "patil", "location": "mumbai", "name": "Prashant"},
+  "E002": {"id": "E002", "last_name": "pant", "location": "NJ", "name": "Rishab"}
+} -->
+
+# Get Employee
+curl -X GET http://127.0.0.1:5000/api/v1/emp/E001
+<!-- {"id": "E001", "last_name": "patil", "location": "mumbai", "name": "Prashant"} -->
+
+
+# update Employee
+curl -d "id=001&name=Rahul&last_name=patil&location=mumbai" -X POST http://127.0.0.1:5000/api/v1/emp/001
+
+<!-- {
+  "001": {"id": "001", "last_name": "patil", "location": "mumbai", "name": "Rahul"},
+  "002": {"id": "002", "last_name": "pant", "location": "NJ", "name": "Rishab"}
+} -->
+
+# Delete Employee
+$ curl  -X DELETE http://127.0.0.1:5000/api/v1/emp/E001
+ 
+<!-- {"E002": {"id": "E002", "last_name": "pant", "location": "NJ", "name": "Rishab"}} -->
+
+# Add Employee
+$ curl -d "id=E001&name=Prashant&last_name=patil&location=mumbai" -X POST http://127.0.0.1:5000/api/v1/emp/E001
+<!-- {
+  "E001": {"id": "E001", "last_name": "patil", "location": "mumbai", "name": "Prashant"},
+  "E002": {"id": "E002", "last_name": "pant", "location": "NJ", "name": "Rishab"}
+} -->
+
+
+# Get TODO
+curl -X GET http://127.0.0.1:5000/api/v1/emp/E001/todo
+<!-- {"data": {"001": {"desc": "create flask api"},"002": {"desc": "create React App"}}} -->
+
+# Update TOTO
+curl -H 'Content-Type: application/json' -X POST -d '{"data": {"001": {"desc": "create flask api"},"002": {"desc": "create React App"}}}' http://127.0.0.1:5000/api/v1/emp/E001/todo
+<!-- {"E001": {"data": {"001": {"desc": "create flask api"},"002": {"desc": "create React App"}}}} -->
+
+# Delete Todo
+curl  -X DELETE http://127.0.0.1:5000/api/v1/emp/E002/todo
---
+++ api.py
@@ -0,0 +1,180 @@
+# api.py
+# Rest API to Select, add, update, delete Employee
+# to Run use > flask run
+#
+#
+from flask_cors import CORS, cross_origin
+from flask import Flask, request
+import time
+import json
+import logging
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO)
+
+# @app.after_request
+# def after_request(response):
+#   response.headers.add('Access-Control-Allow-Origin', '*')
+#   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+#   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+#   return response
+
+
+@app.route('/')
+def home():
+    return '''
+    <h1>API List</h1>
+    <p>http://127.0.0.1:5000/time</p>
+    <p>http://127.0.0.1:5000/api/v1/emp/all</p>
+    <p>http://127.0.0.1:5000/update_emp</p>
+    <p>http://127.0.0.1:5000/delete_emp</p>
+    <p>http://127.0.0.1:5000/get_todo</p>
+    <p>http://127.0.0.1:5000/update_todo</p>
+    <p>http://127.0.0.1:5000/delete_todo</p>
+    '''
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
+
+
+@app.route('/api/v1/emp/<string:id>', methods=['GET'])
+def get_emp(id=None):
+    
+    data = read_json_data(json_name="emp")
+    logging.info("Getting emp data for ID : %s" % id)
+
+    data = data.get(id)
+
+    logging.info("data = %s" % data)
+
+    return data
+
+
+@app.route('/api/v1/emp', methods=['GET', 'OPTIONS'])
+@cross_origin(supports_credentials=True)
+def get_emp_all():
+    logging.info("Getting all employee")
+    data = read_json_data(json_name="emp")
+    return data
+
+
+@app.route('/api/v1/emp/<string:id>', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def update_emp(id):    
+    """Update existing employee or Add new one if not exists
+    curl -d "id=001&name=Rahul&last_name=patil&location=mumbai" -X POST http://127.0.0.1:5000/api/v1/emp/001
+    curl -d "id=E001&name=Prashant&last_name=patil&location=mumbai" -X POST http://127.0.0.1:5000/api/v1/emp/E001
+    """
+    logging.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    logging.info("updating employee %s" % id)
+    logging.info(json.loads(request.data))
+    logging.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+    data = read_json_data(json_name="emp")    
+    emp_data = json.loads(request.data)
+
+    ids = dict([(emp["id"], emp_pos) for emp_pos, emp in enumerate(data["employees"])])
+    logging.info(".......Ids : %s" % ids)
+
+    if id in ids:
+        logging.info("Id exists at position %s" % ids[id])
+        data["employees"].pop(ids[id])
+        data["employees"].insert(ids[id], emp_data)
+
+    update_json_data({"data": data}, json_name="emp")
+    return data
+
+
+@app.route('/api/v1/emp/<string:id>', methods=['DELETE'])
+@cross_origin(supports_credentials=True)
+def delete_emp(id):    
+    logging.info("deleting employee %s" % id)
+    data = read_json_data(json_name="emp")
+    print("==========================")
+    print(data)
+    print("==========================")
+    
+    removed_emp = data.pop(id)
+    logging.info("deleting employee %s" % removed_emp)
+    update_json_data({"data": data}, json_name="emp")
+    return data
+
+
+@app.route('/api/v1/emp/<string:id>/todo', methods=['GET'])
+def get_todo(id=None):
+    query_parameters = request.args
+    data = read_json_data(json_name="todo")
+    logging.info("Getting toto data for ID : %s" % id)
+
+    if query_parameters.get('id'):
+        id = query_parameters.get('id')
+        logging.info("Getting emp data for query ID : %s" % id)
+    
+    data = data.get(id)
+
+    logging.info("data = %s" % data)
+    return data
+
+@app.route('/api/v1/emp/<string:id>/todo', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def update_todo(id):    
+    """Update existing employee or Add new one if not exists
+    curl -d "id=001&name=Rahul&last_name=patil&location=mumbai" -X POST http://127.0.0.1:5000/api/v1/emp/001
+    curl -d "id=E001&name=Prashant&last_name=patil&location=mumbai" -X POST http://127.0.0.1:5000/api/v1/emp/E001
+    """
+    logging.info("updating employee %s" % id)
+    data = read_json_data(json_name="todo")
+    
+    print("==========================")
+    print("==========================")
+    print(json.loads(request.data))
+    print("==========================")
+    
+    todo_data = json.loads(request.data)
+
+    data[id] = todo_data
+
+    update_json_data({"data": data}, json_name="todo")
+    return data
+
+
+@app.route('/api/v1/emp/<string:id>/todo', methods=['DELETE'])
+@cross_origin(supports_credentials=True)
+def delete_todo(id):    
+    logging.info("deleting employee %s" % id)
+    data = read_json_data(json_name="todo")
+    removed_emp = data.pop(id)
+    logging.info("deleting employee %s" % removed_emp)
+    update_json_data({"data": data}, json_name="todo")
+    return data
+
+
+
+def update_json_data(data, json_name):
+    with open("asset/%s.json" % json_name, "w") as json_file:
+        logging.info("Data to be updated in json %s: %s" % (json_name, data))
+        json.dump(data, json_file)
+        logging.info("Successfully changes Json")
+
+
+def read_json_data(json_name):
+    """Read Json data from file
+    """
+    data = {}
+    logging.info("........Reading data from json for %s" % json_name)
+
+    with open("asset/%s.json" % json_name) as emp:
+        data = json.load(emp) 
+        # logging.info("Data from json %s: %s" % (json_name, data))
+
+    logging.info("........Reading data from json completed")
+    return data.get("data")
